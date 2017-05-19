7af091dfb252b91876edf415980ea06665853f9d
adrien-gh <adrien-gh@users.noreply.github.com>
2017-05-19 07:11:05 +0200

    Update Ubuntu_Firewall_Notification.py

--- Ubuntu_Firewall_Notification.py
+++ Ubuntu_Firewall_Notification.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # coding: utf-8
 
+application = 'ufn'
 import gi
 gi.require_version('Notify', '0.7')
 gi.require_version('Gtk', '3.0')
@@ -9,27 +10,26 @@ from parse import compile, parse
 from gi.repository import Notify
 from gi.repository import Gtk
 from subprocess import *
+import gettext
+gettext.install(application)
 
 # Define callback executed on mouse click
-def callback(notif_object, action_name, command):
-    sp = Popen("sudo -i ls /", shell = True, stdin = PIPE, stdout = PIPE, stderr = PIPE)
-    out, err = sp.communicate()
-    print out.rstrip(), err.rstrip()
-    notif_object.close()
-    Gtk.main_quit()
-
 # time = 0 -> add rule permanently
 # time < 0  -> add rule for the session
 # time > 0 -> add rules for time minutes
-def callback2(notif_object, action_name, command, time):
+def callback(notif_object, action_name, command, time):
     if time == 0 :
-    	sp = Popen("gksudo " + command, shell = True, stdout = PIPE, stderr = PIPE)
+        sp = Popen("gksudo " + command, shell = True, stdout = PIPE, stderr = PIPE)
     elif time < 0:
-        sp = Popen("gksudo " + command, shell = True, stdout = PIPE, stderr = PIPE)
+        sp = Popen("gksudo iptables -A " + command, shell = True, stdout = PIPE, stderr = PIPE)
     else:
-        command = "gksudo iptables -A " + command + "; sleep " + str(60 * time) + "; sudo iptables -D " + command
-        sp = Popen("gksudo " + command, shell = True, stdout = PIPE, stderr = PIPE)
-    print command
+        tmpFile = open("/tmp/ufn.dat", "w")
+        tmpFile.write("gksudo iptables -D " + command)
+        tmpFile.close()
+        
+        command = "gksudo iptables -A " + command #+ "; at now +" + str(time) + " minutes -f /tmp/ufn.dat"
+        sp = Popen(command, shell = True, stdout = PIPE, stderr = PIPE)
+        sp = Popen("at now +" + str(time) + " minutes -f /tmp/ufn.dat", shell = True, stdout = PIPE, stderr = PIPE)
     out, err = sp.communicate()
     print out.rstrip(), err.rstrip()
     notif_object.close()
@@ -59,47 +59,47 @@ try:
 
         commandIptables = "INPUT -p " + proto + " -i " + interface + " -s " + src + " -d " + dst
         if dpt != None :
-            commadIptables = commandIptables + " --dport " + dpt
+            commandIptables = commandIptables + " --dport " + dpt
         commandIptables = commandIptables + " -j ACCEPT"
 
-        commandUfw = "gksudo ufw allow from " + interface + " to " + out + " proto " + proto
+        commandUfw = "ufw allow from " + interface + " to " + out + " proto " + proto
         if dpt != None :
             commandUfw = commandUfw + " port " + dpt
 
         Notify.init('Ubuntu Firewall Notification')
         notif = Notify.Notification.new(
-            "A packet has been bloqued by the firewall", # title
-            "command", # message
+            _("Ubuntu Firewall Notification"), # title
+            _("A packet has been bloqued by the firewall"), # message
             'dialog-information' # icon
         )
         # add the custom notification action
         notif.add_action(
-            'our_callback',
-            'Allow traffic permanently', # Button text
-            callback2, # function callback de notre bouton
+            'permanent',
+            _('Allow traffic permanently'), # Button text
+            callback, # function callback de notre bouton
             commandUfw, # fonction qui supprime les user_datas
             0
         )
         notif.add_action(
-            'our_callback',
-            'Allow traffic for the session', # Button text
-            callback2, # function callback de notre bouton
+            'session',
+            _('Allow traffic for the session'), # Button text
+            callback, # function callback de notre bouton
             # None, # user_data, required data for the callback, For now: nothing
             commandIptables, # fonction qui supprime les user_datas
             -1
         )
         notif.add_action(
-            'our_callback',
-            'Allow traffic for 30 minutes', # Button text
-            callback2, # function callback de notre bouton
+            '30minutes',
+            _('Allow traffic for 30 minutes'), # Button text
+            callback, # function callback de notre bouton
             # None, # user_data, required data for the callback, For now: nothing
             commandIptables, # fonction qui supprime les user_datas
             30
         )
         notif.add_action(
-            'our_callback',
-            'Allow traffic for 1 hour', # Button text
-            callback2, # function callback de notre bouton
+            '60minutes',
+            _('Allow traffic for 1 hour'), # Button text
+            callback, # function callback de notre bouton
             # None, # user_data, required data for the callback, For now: nothing
             commandIptables, # fonction qui supprime les user_datas
             60
